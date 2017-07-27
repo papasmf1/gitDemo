08bc5fb8c4cf60f176809a29fcd56bca5bb9f560
jong Kim <jongdeokkim@70-12-246-118.pools.spcsdns.net>
2017-07-26 18:33:54 -0700

    Map파일을 추가함

---
+++ Git3/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  Git3
+//
+//  Created by jong deok Kim on 2017. 7. 26..
+//  Copyright © 2017년 credu. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
